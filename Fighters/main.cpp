279cf96b14bea5bd44d68327822b54d9e207df2f
Jimmy Fan <fjm@JimmydeMacBook-Air.local>
2017-05-11 11:22:10 +0800

    Initial Commit

---
+++ Fighters/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Fighters
+//
+//  Created by Jimmy Fan on 2017/5/11.
+//  Copyright © 2017年 Jimmy Fan. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
